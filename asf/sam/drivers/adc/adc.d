9673897983737a5dd91fa5addb41b9f47fa69cb0
wrsna <malnsck@gmail.com>
2024-01-09 14:54:26 +0100

    library cod from arm

---
+++ asf/sam/drivers/adc/adc.d
@@ -0,0 +1,346 @@
+asf/sam/drivers/adc/adc.o: asf/sam/drivers/adc/adc.c \
+ asf/sam/drivers/adc/adc.h asf/sam/utils/compiler.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h \
+ asf/common/utils/parts.h asf/sam/utils/preprocessor/preprocessor.h \
+ asf/sam/utils/preprocessor/tpaste.h asf/sam/utils/preprocessor/stringz.h \
+ asf/sam/utils/preprocessor/mrepeat.h \
+ asf/sam/utils/preprocessor/preprocessor.h \
+ asf/sam/utils/header_files/io.h \
+ asf/sam/utils/cmsis/sam3x/include/sam3xa.h \
+ asf/sam/utils/cmsis/sam3x/include/sam3x8e.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h \
+ asf/thirdparty/CMSIS/include/core_cm3.h \
+ asf/thirdparty/CMSIS/include/core_cmInstr.h \
+ asf/thirdparty/CMSIS/include/core_cmFunc.h \
+ asf/sam/utils/cmsis/sam3x/source/templates/system_sam3x.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_adc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_can.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_chipid.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_dacc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_dmac.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_efc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_emac.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_gpbr.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_hsmci.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_matrix.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_pdc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_pio.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_pmc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_pwm.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_rstc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_rtc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_rtt.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_smc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_spi.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_ssc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_supc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_tc.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_trng.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_twi.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_uart.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_uotghs.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_usart.h \
+ asf/sam/utils/cmsis/sam3x/include/component/component_wdt.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_hsmci.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_ssc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_spi0.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_tc0.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_tc1.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_tc2.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_twi0.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_twi1.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_pwm.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_usart0.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_usart1.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_usart2.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_usart3.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_uotghs.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_emac.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_can0.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_can1.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_trng.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_adc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_dmac.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_dacc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_smc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_matrix.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_pmc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_uart.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_chipid.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_efc0.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_efc1.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_pioa.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_piob.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_pioc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_piod.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_rstc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_supc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_rtt.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_wdt.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_rtc.h \
+ asf/sam/utils/cmsis/sam3x/include/instance/instance_gpbr.h \
+ asf/sam/utils/cmsis/sam3x/include/pio/pio_sam3x8e.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/cdefs.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /usr/include/newlib/alloca.h asf/common/utils/interrupt.h \
+ asf/common/utils/interrupt/interrupt_sam_nvic.h \
+ asf/sam/utils/status_codes.h
+
+asf/sam/drivers/adc/adc.h:
+
+asf/sam/utils/compiler.h:
+
+/usr/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h:
+
+asf/common/utils/parts.h:
+
+asf/sam/utils/preprocessor/preprocessor.h:
+
+asf/sam/utils/preprocessor/tpaste.h:
+
+asf/sam/utils/preprocessor/stringz.h:
+
+asf/sam/utils/preprocessor/mrepeat.h:
+
+asf/sam/utils/preprocessor/preprocessor.h:
+
+asf/sam/utils/header_files/io.h:
+
+asf/sam/utils/cmsis/sam3x/include/sam3xa.h:
+
+asf/sam/utils/cmsis/sam3x/include/sam3x8e.h:
+
+/usr/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+asf/thirdparty/CMSIS/include/core_cm3.h:
+
+asf/thirdparty/CMSIS/include/core_cmInstr.h:
+
+asf/thirdparty/CMSIS/include/core_cmFunc.h:
+
+asf/sam/utils/cmsis/sam3x/source/templates/system_sam3x.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_adc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_can.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_chipid.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_dacc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_dmac.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_efc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_emac.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_gpbr.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_hsmci.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_matrix.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_pdc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_pio.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_pmc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_pwm.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_rstc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_rtc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_rtt.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_smc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_spi.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_ssc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_supc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_tc.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_trng.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_twi.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_uart.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_uotghs.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_usart.h:
+
+asf/sam/utils/cmsis/sam3x/include/component/component_wdt.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_hsmci.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_ssc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_spi0.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_tc0.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_tc1.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_tc2.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_twi0.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_twi1.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_pwm.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_usart0.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_usart1.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_usart2.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_usart3.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_uotghs.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_emac.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_can0.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_can1.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_trng.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_adc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_dmac.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_dacc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_smc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_matrix.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_pmc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_uart.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_chipid.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_efc0.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_efc1.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_pioa.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_piob.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_pioc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_piod.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_rstc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_supc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_rtt.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_wdt.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_rtc.h:
+
+asf/sam/utils/cmsis/sam3x/include/instance/instance_gpbr.h:
+
+asf/sam/utils/cmsis/sam3x/include/pio/pio_sam3x8e.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/newlib.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/machine/endian.h:
+
+/usr/include/newlib/machine/_endian.h:
+
+/usr/include/newlib/sys/select.h:
+
+/usr/include/newlib/sys/_sigset.h:
+
+/usr/include/newlib/sys/_timeval.h:
+
+/usr/include/newlib/sys/timespec.h:
+
+/usr/include/newlib/sys/_timespec.h:
+
+/usr/include/newlib/sys/_pthreadtypes.h:
+
+/usr/include/newlib/sys/sched.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
+
+/usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
+
+/usr/include/newlib/stdlib.h:
+
+/usr/include/newlib/machine/stdlib.h:
+
+/usr/include/newlib/alloca.h:
+
+asf/common/utils/interrupt.h:
+
+asf/common/utils/interrupt/interrupt_sam_nvic.h:
+
+asf/sam/utils/status_codes.h:
